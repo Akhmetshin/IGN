6f555df97a7fdc674ed56edebb2725afa91fa80a
Akhmetshin <renat.new@gmail.com>
2020-03-07 08:32:03 +0300

    Fix1

--- IGNView/Form1.cs
+++ IGNView/Form1.cs
@@ -23,20 +23,15 @@ namespace IGNView
         float[] depth = new float[3199];
         float[,] data = new float[3199, 43];
 
-        PointF[] pf;
-        PointF[] pfT;
-
         int h0;
         public IGNView()
         {
             InitializeComponent();
 
             h0 = Height / 3 * 2;
-            pf = new PointF[43];
-            pfT = new PointF[43];
 
             SolidBrushB = new SolidBrush(BackColor);
-            r = new Rectangle(0, 0, Width - 100, h0);
+            r = new Rectangle(0, 0, Width - 100, h0 + 1);
 
             graf = this.CreateGraphics();
             pen = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
@@ -88,16 +83,124 @@ namespace IGNView
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
+            float[] masOrig = new float[43];
+            float[] masWork = new float[43];
+            float[] masFar = new float[43];
+            float[] masNear = new float[43];
+            float[] masFAN = new float[43];
+
+            double U0_O = 0;
+            double T2_O = 0;
+            double S_O = 0;
+            double U0_W = 0;
+            double T2_W = 0;
+            double S_W = 0;
+            double U0_F = 0;
+            double T2_F = 0;
+            double S_F = 0;
+            double U0_N = 0;
+            double T2_N = 0;
+            double S_N = 0;
+            double U0_FAN = 0;
+            double T2_FAN = 0;
+            double S_FAN = 0;
+
             label1.Text = depth[trackBar1.Value].ToString();
             graf.FillRectangle(SolidBrushB, r);
+            
+            for (int j = 0; j < 43; j++) masOrig[j] = data[trackBar1.Value, j];
+            DrawExp(43, masOrig, pen);
 
-            for (int j = 0; j < 43; j++)
+            MinSquareMas(43, masOrig, ref U0_O, ref T2_O, ref S_O);
+            DrawExp(43, masWork, Pens.Red, U0_O, T2_O);
+            
+            int n = 0;
+        }
+        private void DrawExp(int n, float[] mas, Pen p, double u, double t)
+        {
+            PointF[] pf = new PointF[43];
+            for (int j = 0; j < n; j++)
+            {
+                mas[j]= (float)(u * Math.Exp(-(j * 2) / t));
+            }
+            for (int j = 0; j < n; j++)
             {
                 pf[j].X = j * 20;
-                pf[j].Y = h0 - (data[trackBar1.Value, j] / 50);
+                pf[j].Y = h0 - mas[j] / 50;
             }
-            graf.DrawLines(pen, pf);
+            graf.DrawLines(p, pf);
+        }
+        private void DrawExp(int n, float[] mas, Pen p)
+        {
+            PointF[] pf = new PointF[43];
 
+            for (int j = 0; j < n; j++)
+            {
+                pf[j].X = j * 20;
+                pf[j].Y = h0 - mas[j] / 50;
+            }
+            graf.DrawLines(p, pf);
+        }
+        private static int MinSquareMas(int n, float[] mas, ref double u, ref double t, ref double s)
+        {
+            double x, y, m;
+            double sum1, sum2, sum3, sum4;
+
+            sum1 = sum2 = sum3 = sum4 = 0;
+            m = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                y = Math.Log(mas[i]);
+                m++;
+
+                sum1 += x * y;
+                sum2 += x;
+                sum3 += y;
+                sum4 += x * x;
+            }
+
+            if (m < 3)
+            {
+                u = -1;
+                t = -1;
+                s = -1;
+                return -1;
+            }
+
+            double b = (sum2 * sum3 - m * sum1) / (sum2 * sum2 - m * sum4);
+            double a = (sum3 - b * sum2) / m;
+
+            u = Math.Exp(a);
+            t = -1 / b;
+
+            if (double.IsInfinity(u)) return -1;
+            if (double.IsInfinity(t)) return -1;
+
+            if (0 > u) return -1;
+            if (0 > t) return -1;
+
+            sum1 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                y = Math.Log(mas[i]);
+                m++;
+                sum1 += (y - a - b * x) * (y - a - b * x);
+            }
+
+            if (m > 2)
+            {
+                s = sum1 / (m - 2);
+            }
+            else { s = 0; }
+
+            return 0;
         }
     }
 }
