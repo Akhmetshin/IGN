173366cd36e50fb4cf671ca6a0106708ea8db2a8
Akhmetshin <renat.new@gmail.com>
2020-03-16 19:32:00 +0300

    IGNViewNew3. продолжаю экспериментировать

---
+++ IGNViewNew3/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace IGNViewNew3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(724, 426);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 494);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(350, 248);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(423, 45);
+            this.trackBar2.TabIndex = 6;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 462);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(804, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 563);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ IGNViewNew3/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IGNViewNew3
+{
+    public partial class Form1 : Form
+    {
+        Graphics graf;
+        Pen pen1;
+        Pen pen2;
+        SolidBrush SolidBrushB;
+        Rectangle r;
+
+        float[] depth = new float[3199];
+        float[,] data = new float[3199, 43];
+
+        int h0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            h0 = Height / 3 * 2;
+
+            SolidBrushB = new SolidBrush(BackColor);
+            r = new Rectangle(0, 0, Width - 100, h0 + 1);
+
+            graf = this.CreateGraphics();
+            pen1 = new Pen(Color.FromArgb(255, 0, 0, 0), 2);
+            pen2 = new Pen(Color.FromArgb(255, 0, 255, 255), 2);
+
+            File();
+        }
+
+        private void File()
+        {
+            var culture = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            string line;
+            string[] w;
+            w = new string[43];
+            char[] delimiterChars = { ' ', '\t', ';' };
+            int i = 0;
+            float fd = 0;
+            try
+            {
+                StreamReader sr = new StreamReader(@"D:\MyProgect\IGN\1017_спады 30.txt");
+                line = sr.ReadLine();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    w = line.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                    if (w.Length > 0)
+                    {
+                        fd = float.Parse(w[0], NumberStyles.Float | NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent, CultureInfo.InvariantCulture);
+                        depth[i] = fd;
+                        for (int j = 0; j < 43; j++)
+                        {
+                            fd = float.Parse(w[j + 1], NumberStyles.Float | NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent, CultureInfo.InvariantCulture);
+                            data[i, j] = fd;
+                        }
+                        i++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            trackBar1.Maximum = i - 1;
+            trackBar1.TickFrequency = 50;
+
+            trackBar2.Minimum = 3;
+            trackBar2.Maximum = 40;
+            trackBar2.TickFrequency = 5;
+        }
+        private void MainDraw()
+        {
+            double[] masOrig = new double[45]; // !!! 45 - это для поиска jMax !!!
+            double[] mas1 = new double[43];
+            double[] mas2 = new double[43];
+
+            label1.Text = depth[trackBar1.Value].ToString();
+            label2.Text = trackBar2.Value.ToString();
+
+            graf.FillRectangle(SolidBrushB, r);
+
+            for (int j = 0; j < 45; j++) masOrig[j] = 0;
+            for (int j = 0; j < 43; j++) mas1[j] = 0;
+            for (int j = 0; j < 43; j++) masOrig[j] = data[trackBar1.Value, j];
+
+            double U0_O = 0;
+            double T2_O = 0;
+            double S_O = 0;
+            double U0_W = 0;
+            double T2_W = 0;
+            double S_W = 0;
+
+            int offset = 0;
+            int tailLen = 7;
+            double U0 = 0, T2 = 0, S = 0;
+            double U0_L = 0, T2_L = 0, S_L = 0;
+            for (int n = 0; n < 43 - tailLen; n++)
+            {
+                for (int j = 0; j < tailLen; j++) mas1[j] = masOrig[j + offset];
+                if (-1 == MinSquareMas(tailLen, mas1, ref U0, ref T2, ref S)) break;
+                U0_O = U0;
+                T2_O = T2;
+                S_O = S;
+                if (-1 == MinSquareMasLin(tailLen, mas1, ref U0, ref T2, ref S)) break;
+                U0_L = U0;
+                T2_L = T2;
+                S_L = S;
+                if (S_O > S_L) break;
+                offset++;
+            }
+
+            int jMax = offset;
+            trackBar2.Maximum = jMax - 3;
+            label3.Text = jMax.ToString();
+
+            for (int j = 0; j < trackBar2.Value; j++) mas1[j] = masOrig[j];
+            int l = 0;
+            for (int j = trackBar2.Value - 1; j < jMax; j++) { mas2[l] = masOrig[j]; l++; }
+
+            PointF[] pf1 = new PointF[trackBar2.Value];
+            PointF[] pf2 = new PointF[jMax - trackBar2.Value];
+
+            for (int j = 0; j < trackBar2.Value; j++)
+            {
+                pf1[j].X = j * 20;
+                pf1[j].Y = (float)(h0 - mas1[j] / 50);
+            }
+            graf.DrawLines(pen1, pf1);
+
+            for (int j = 0; j < jMax - trackBar2.Value; j++)
+            {
+                pf2[j].X = (j + trackBar2.Value - 1) * 20;
+                pf2[j].Y = (float)(h0 - mas2[j] / 50);
+            }
+            if (jMax - trackBar2.Value > 1) graf.DrawLines(pen2, pf2);
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            MainDraw();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            MainDraw();
+        }
+        private static int MinSquareMas(int n, double[] mas, ref double u, ref double t, ref double s)
+        {
+            double x, y, m;
+            double sum1, sum2, sum3, sum4;
+
+            sum1 = sum2 = sum3 = sum4 = 0;
+            m = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                y = Math.Log(mas[i]);
+                m++;
+
+                sum1 += x * y;
+                sum2 += x;
+                sum3 += y;
+                sum4 += x * x;
+            }
+
+            if (m < 3)
+            {
+                u = -1;
+                t = -1;
+                s = -1;
+                return -1;
+            }
+
+            double b = (sum2 * sum3 - m * sum1) / (sum2 * sum2 - m * sum4);
+            double a = (sum3 - b * sum2) / m;
+
+            u = Math.Exp(a);
+            t = -1 / b;
+
+            if (double.IsInfinity(u)) return -1;
+            if (double.IsInfinity(t)) return -1;
+
+            if (0 > u) return -1;
+            if (0 > t) return -1;
+
+            sum1 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                y = Math.Log(mas[i]);
+                m++;
+                sum1 += (y - a - b * x) * (y - a - b * x);
+            }
+
+            if (m > 2)
+            {
+                s = sum1 / (m - 2);
+            }
+            else { s = 0; }
+
+            return 0;
+        }
+        private static int MinSquareMasLin(int n, double[] mas, ref double u, ref double t, ref double s)
+        {
+            double x, y, m;
+            double sum1, sum2, sum3, sum4;
+
+            sum1 = sum2 = sum3 = sum4 = 0;
+            m = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                //y = Math.Log(mas[i]);
+                y = mas[i];
+                m++;
+
+                sum1 += x * y;
+                sum2 += x;
+                sum3 += y;
+                sum4 += x * x;
+            }
+
+            if (m < 3)
+            {
+                u = -1;
+                t = -1;
+                s = -1;
+                return -1;
+            }
+
+            double b = (sum2 * sum3 - m * sum1) / (sum2 * sum2 - m * sum4);
+            double a = (sum3 - b * sum2) / m;
+
+            //u = Math.Exp(a);
+            u = a;
+            t = -1 / b;
+
+            if (double.IsInfinity(u)) return -1;
+            if (double.IsInfinity(t)) return -1;
+
+            if (0 > u) return -1;
+            if (0 > t) return -1;
+
+            sum1 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (mas[i] < 0.001) continue;
+
+                x = i * 2;
+                //y = Math.Log(mas[i]);
+                y = mas[i];
+                m++;
+                sum1 += (y - a - b * x) * (y - a - b * x);
+            }
+
+            if (m > 2)
+            {
+                s = sum1 / (m - 2);
+            }
+            else { s = 0; }
+
+            return 0;
+        }
+    }
+}
